9ea3fffd8b7dd6063349da7b8739158a49ea9886
Mamabolo Katlego <rabotaikatlego@gmail.com>
2024-07-03 02:28:07 +0000

    all files

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -1,37 +1,39 @@
 #!/usr/bin/env python3
-"""
-Module 0-app
-"""
+""" module 4-app.py with a basic Flask app and Babel setup """
+
 from flask import Flask, render_template, request
 from flask_babel import Babel
 
+app = Flask(__name__)
+""" instantiates Babel object """
+babel = Babel(app)
+
 
 class Config(object):
-    """i18n configuration"""
+    """ config class for Babel """
     LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
-app = Flask(__name__)
+""" set above class object as the configuration for the app """
 app.config.from_object(Config)
-babel = Babel(app)
-
-
-@app.route("/", strict_slashes=False)
-def index():
-    """didplays a basic hello world message"""
-    return render_template("4-index.html")
 
 
 @babel.localeselector
 def get_locale():
-    """Gets best fmatch locale according to request"""
-    locale = request.args.get('locale')
-    if locale and locale in app.config['LANGUAGES']:
-        return locale
+    """ gets best match for supported languages in locales """
+    if 'locale' in request.args and request.args.get(
+            'locale') in app.config['LANGUAGES']:
+        return request.args.get('locale')
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port="5000")
+@app.route('/', strict_slashes=False)
+def index() -> str:
+    """ Basic Flask App """
+    return render_template('4-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
